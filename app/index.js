17d34a4ee6a8c9ec5d568c04906918cefb8331eb
Tien Pham <tiendev94@gmail.com>
2020-01-11 21:13:35 +0200

    toggleTheme

--- app/index.js
+++ app/index.js
@@ -13,6 +13,27 @@ const Results = React.lazy(() => import('./components/Results'))
 const App = () => {
   const [theme, setTheme] = React.useState('light') 
   const toggleTheme = () => setTheme((theme) => theme === 'light' ? 'dark' : 'light')
+
+  return (
+    <Router>
+      <ThemeProvider value={theme}>
+        <div className={theme}>
+          <div className='container'>
+            <Nav toggleTheme={toggleTheme}/>
+
+            <React.Suspense fallback={<Loading />} >
+              <Switch>
+                <Route exact path='/' component={Popular} />
+                <Route exact path='/battle' component={Battle} />
+                <Route path='/battle/results' component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </React.Suspense>
+          </div>
+        </div>
+      </ThemeProvider>
+    </Router>
+  )
 }
 
 class App extends React.Component {
@@ -25,26 +46,7 @@ class App extends React.Component {
     }
   }
   render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className='container'>
-              <Nav />
-
-              <React.Suspense fallback={<Loading />} >
-                <Switch>
-                  <Route exact path='/' component={Popular} />
-                  <Route exact path='/battle' component={Battle} />
-                  <Route path='/battle/results' component={Results} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
-          </div>
-        </ThemeProvider>
-      </Router>
-    )
+    
   }
 }
 
